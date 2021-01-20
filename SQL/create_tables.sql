1e8ed74219b92b18e30bbed1431d6467316fa1c7
dkumar09 <onlydeepender@gmail.com>
2021-01-20 11:37:10 +0530

    Adds order transaction and order_item table

--- SQL/create_tables.sql
+++ SQL/create_tables.sql
@@ -31,7 +31,7 @@ CREATE TABLE `psycart`.`product`(
     `price` FLOAT NOT NULL DEFAULT 0,
     `discount` FLOAT NOT NULL DEFAULT 0,
     `quantity`SMALLINT(6) NOT NULL DEFAULT 0,
-    `shop` TINYINT(1) NOT NULL DEFAULT 0,
+    `psycart` TINYINT(1) NOT NULL DEFAULT 0,
     `createdAt` DATETIME NOT NULL,
     `updatedAt` DATETIME NULL DEFAULT NULL,
     `publishedAt` DATETIME NULL DEFAULT NULL,
@@ -159,4 +159,123 @@ CREATE TABLE `psycart`.`cart` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 --CART ITEM
-CREATE TABLE `psycart`.`cart_item`
+CREATE TABLE `psycart`.`cart_item`(
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `productId` BIGINT NOT NULL,
+    `cartId` BIGINT NOT NULL,
+    `sku` VARCHAR(100) NOT NULL,
+    `price` FLOAT NOT NULL DEFAULT 0,
+    `discount` FLOAT NOT NULL DEFAULT 0,
+    `quantity` SMALLINT(6) NOT NULL DEFAULT 0,
+    `active` TINYINT(1) NOT NULL DEFAULT 0,
+    `createdAt` DATETIME NOT NULL,
+    `updatedAt` DATETIME NOT NULL,
+    `content` TEXT NULL DEFAULT NULL,
+    PRIMARY KEY(`id`),
+    INDEX `idx_cart_item_product` (`productId` ASC),
+    CONSTRAINT `fk_cart_item_product`
+        FOREIGN KEY(`productId`)
+        REFERENCES `psycart`.`product` (`id`)
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+);
+ALTER TABLE `psycart`.`cart_items`
+ADD INDEX `idx_cart_item_cart` (`cartId` ASC);
+ALTER TABLE `psycart`.`cart_item`
+ADD CONSTRAINT `fk_cart_item_cart`
+    FOREIGN KEY(`cartId`)
+    REFERENCES `psycart`.`cart` (`id`)
+    ON UPDATE NO ACTION
+    On DELETE NO ACTION;
+
+--ORDER
+CREATE TABLE `psycart`.`order` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `userId` BIGINT NULL DEFAULT NULL,
+  `sessionId` VARCHAR(100) NOT NULL,
+  `token` VARCHAR(100) NOT NULL,
+  `status` SMALLINT(6) NOT NULL DEFAULT 0,
+  `subTotal` FLOAT NOT NULL DEFAULT 0,
+  `itemDiscount` FLOAT NOT NULL DEFAULT 0,
+  `tax` FLOAT NOT NULL DEFAULT 0,
+  `shipping` FLOAT NOT NULL DEFAULT 0,
+  `total` FLOAT NOT NULL DEFAULT 0,
+  `promo` VARCHAR(50) NULL DEFAULT NULL,
+  `discount` FLOAT NOT NULL DEFAULT 0,
+  `grandTotal` FLOAT NOT NULL DEFAULT 0,
+  `firstName` VARCHAR(50) NULL DEFAULT NULL,
+  `middleName` VARCHAR(50) NULL DEFAULT NULL,
+  `lastName` VARCHAR(50) NULL DEFAULT NULL,
+  `mobile` VARCHAR(15) NULL,
+  `email` VARCHAR(50) NULL,
+  `line1` VARCHAR(50) NULL DEFAULT NULL,
+  `line2` VARCHAR(50) NULL DEFAULT NULL,
+  `city` VARCHAR(50) NULL DEFAULT NULL,
+  `province` VARCHAR(50) NULL DEFAULT NULL,
+  `country` VARCHAR(50) NULL DEFAULT NULL,
+  `createdAt` DATETIME NOT NULL,
+  `updatedAt` DATETIME NULL DEFAULT NULL,
+  `content` TEXT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `idx_order_user` (`userId` ASC),
+  CONSTRAINT `fk_order_user`
+    FOREIGN KEY (`userId`)
+    REFERENCES `psycart`.`user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+CREATE TABLE `psycart`.`order_item` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `productId` BIGINT NOT NULL,
+  `orderId` BIGINT NOT NULL,
+  `sku` VARCHAR(100) NOT NULL,
+  `price` FLOAT NOT NULL DEFAULT 0,
+  `discount` FLOAT NOT NULL DEFAULT 0,
+  `quantity` SMALLINT(6) NOT NULL DEFAULT 0,
+  `createdAt` DATETIME NOT NULL,
+  `updatedAt` DATETIME NULL DEFAULT NULL,
+  `content` TEXT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `idx_order_item_product` (`productId` ASC),
+  CONSTRAINT `fk_order_item_product`
+    FOREIGN KEY (`productId`)
+    REFERENCES `psycart`.`product` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+ALTER TABLE `psycart`.`order_item` 
+ADD INDEX `idx_order_item_order` (`orderId` ASC);
+ALTER TABLE `psycart`.`order_item` 
+ADD CONSTRAINT `fk_order_item_order`
+  FOREIGN KEY (`orderId`)
+  REFERENCES `psycart`.`order` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+--TRANSACTION
+CREATE TABLE `psycart`.`transaction` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `userId` BIGINT NOT NULL,
+  `orderId` BIGINT NOT NULL,
+  `code` VARCHAR(100) NOT NULL,
+  `type` SMALLINT(6) NOT NULL DEFAULT 0,
+  `mode` SMALLINT(6) NOT NULL DEFAULT 0,
+  `status` SMALLINT(6) NOT NULL DEFAULT 0,
+  `createdAt` DATETIME NOT NULL,
+  `updatedAt` DATETIME NULL DEFAULT NULL,
+  `content` TEXT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `idx_transaction_user` (`userId` ASC),
+  CONSTRAINT `fk_transaction_user`
+    FOREIGN KEY (`userId`)
+    REFERENCES `psycart`.`user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+ALTER TABLE `psycart`.`transaction` 
+ADD INDEX `idx_transaction_order` (`orderId` ASC);
+ALTER TABLE `psycart`.`transaction` 
+ADD CONSTRAINT `fk_transaction_order`
+  FOREIGN KEY (`orderId`)
+  REFERENCES `psycart`.`order` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
